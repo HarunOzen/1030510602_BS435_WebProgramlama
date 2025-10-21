499669b090435b952788f977ab5e58933f6785cf
HarunOzen <1030510602@erciyes.edu.tr>
2025-10-22 01:50:04 +0300

    react projesi kuruldu ve gereksiz dosyalar temizlendi

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+
+import './App.css'; // CSS dosyasını içeri aktarıyoruz
+
+// Bu bir "Fonksiyonel Component"tir (Class değil)
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Yapay Zeka Görseli Bulma Oyunu</h1>
+         <p>temel iskeleti oluşturdum haftya tekrar commit atarım.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
